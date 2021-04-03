0b6b42d5a95d5fb8b061884ba3504c675c201469
buryattvoydrug <buryattvoydrug@gmail.com>
2021-04-03 17:53:30 +0300

    category get, create, delete, update

---
+++ controllers/categoryCtrl.js
@@ -0,0 +1,54 @@
+const Category = require("../models/categoryModel")
+
+
+const categoryCtrl = {
+  getCategories: async(req,res)=>{
+    //res.json('Category test ctrl')
+    try{
+      const categories = await Category.find()
+      res.json(categories)
+
+
+    } catch (err){
+      return res.status(500).json({msg:err.message})
+    }
+  },
+  createCategory:async(req,res)=>{
+    try{
+      const {name} = req.body
+      const category = await Category.findOne({name})
+      if (category) return res.status(400).json({msg:"This category already exists."})
+
+      const newCategory = new Category({name})
+
+      await newCategory.save()
+      res.json({msg:'Created a category'})
+      // res.json('Check admin success')
+    } catch (err){
+      return res.status(500).json({msg:err.message})
+    }
+  },
+  deleteCategory:async(req,res)=>{
+    try{
+
+      await Category.findByIdAndDelete(req.params.id)
+      res.json({msg:"Deleted a category"})
+
+    } catch (err){
+      return res.status(500).json({msg:err.message})
+    }
+  },
+  updateCategory:async(req,res)=>{
+    try{
+      const {name}=req.body
+      await Category.findByIdAndUpdate({_id:req.params.id},{name})
+
+      res.json({msg:"Updated category"})
+
+    } catch (err){
+      return res.status(500).json({msg:err.message})
+    }
+  }
+}
+
+module.exports = categoryCtrl
